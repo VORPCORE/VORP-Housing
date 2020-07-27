e4b75e34c661b8bbddde4a553aae341f013858a7
Osmany González <devbiitz2@gmail.com>
2020-07-27 01:26:52 +0100

    First Init

---
+++ VORP-Housing[Client-Server]/vorphousing_cl/GetConfig.cs
@@ -0,0 +1,44 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace vorphousing_cl
+{
+    public class GetConfig : BaseScript
+    {
+        public static JObject Config = new JObject();
+        public static JArray CharactArray = new JArray();
+        public static Dictionary<string, string> Langs = new Dictionary<string, string>();
+        public static bool isLoaded = false;
+
+        public GetConfig()
+        {
+            EventHandlers[$"{API.GetCurrentResourceName()}:SendConfig"] += new Action<string, ExpandoObject>(LoadDefaultConfig);
+            TriggerServerEvent($"{API.GetCurrentResourceName()}:getConfig");
+        }
+
+        private void LoadDefaultConfig(string dc, ExpandoObject dl)
+        {
+            Config = JObject.Parse(dc);
+
+            foreach (var l in dl)
+            {
+                Langs[l.Key] = l.Value.ToString();
+            }
+
+
+            isLoaded = true;
+        }
+    }
+}
---
+++ VORP-Housing[Client-Server]/vorphousing_cl/vorphousing_cl_init.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vorphousing_cl
+{
+    public class vorphousing_cl_init
+    {
+    }
+}
