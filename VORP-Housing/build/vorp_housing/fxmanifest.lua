bc91f36460a78ff3f6c87ce605dcdc0b091c92ff
SimpleSandman <simple.sandman@gmail.com>
2022-04-24 19:14:19 -0500

    Added missing config.json file and added wildcards for script DLLs

--- VORP-Housing/build/vorp_housing/fxmanifest.lua
+++ VORP-Housing/build/vorp_housing/fxmanifest.lua
@@ -3,10 +3,10 @@ game 'rdr3'
 fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-client_scripts { 'VORP.Housing.Client.net.dll' }
-server_scripts { 'VORP.Housing.Server.net.dll' }
+client_scripts { '*.Client.net.dll' }
+server_scripts { '*.Server.net.dll' }
 
-files { 'Newtonsoft.Json.dll' }
+files { 'Newtonsoft.Json.dll', 'config.json' }
 
 -- Log Levels; none, trace, debug, info, warn, error, all
 log_level 'all'
