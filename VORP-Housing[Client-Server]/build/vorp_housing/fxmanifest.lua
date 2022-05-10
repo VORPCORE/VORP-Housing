3abe1ff7883864569f6c2b2cd45d0aa6acbfeb51
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-05-10 18:58:16 +0100

    Version check

--- VORP-Housing[Client-Server]/build/vorp_housing/fxmanifest.lua
+++ VORP-Housing[Client-Server]/build/vorp_housing/fxmanifest.lua
@@ -10,3 +10,9 @@ files { 'Newtonsoft.Json.dll' }
 
 -- Log Levels; none, trace, debug, info, warn, error, all
 log_level 'none'
+
+--dont touch
+version '1.0'
+vorp_checker 'yes'
+vorp_name '^4Resource version Check^3'
+vorp_github 'https://github.com/VORPCORE/VORP-Housing'
